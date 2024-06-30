88f1955f1c79d0180fbffd881c98a5b9908250c2
josemarialanda <josemaria.landa@gmail.com>
2024-06-30 22:52:55 +0100

    allow unfree

--- hosts/homelab/home-manager/home.nix
+++ hosts/homelab/home-manager/home.nix
@@ -10,39 +10,53 @@
     ../../common/home
   ];
 
-    home.packages = with pkgs; [
-      # CLI tools.
-      podman-compose
-      appimage-run
-      bottom
-      pfetch
-      ssh-tools
-      wget
-      ffmpeg
-      ripgrep
-      tree
-      multitail
-      jq
-      fx
-      jp
-      yq
-      up
-      rmlint
-      with-shell
-      steam-run
-      trash-cli
-      retry
-      concurrently
-      http-prompt
-      detox
-      git-sync
-      sox
-      catimg
-      bc
-      clac
-      rlwrap
-      wtf
-      pipr
+  nixpkgs = {
+    overlays = [
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
     ];
+    config = {
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = _: true;
+      permittedInsecurePackages = [ ];
+    };
+  };
+
+  home.packages = with pkgs; [
+    # CLI tools.
+    podman-compose
+    appimage-run
+    bottom
+    pfetch
+    ssh-tools
+    wget
+    ffmpeg
+    ripgrep
+    tree
+    multitail
+    jq
+    fx
+    jp
+    yq
+    up
+    rmlint
+    with-shell
+    steam-run
+    trash-cli
+    retry
+    concurrently
+    http-prompt
+    detox
+    git-sync
+    sox
+    catimg
+    bc
+    clac
+    rlwrap
+    wtf
+    pipr
+  ];
 
 }
