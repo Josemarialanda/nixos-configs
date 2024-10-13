3671ebd702f8abaaf7032ab45366980ce55b8d81
josemarialanda <josemaria.landa@gmail.com>
2024-10-13 15:27:24 +0100

    update inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Your new nix config";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager.url = "github:nix-community/home-manager/release-24.05";
