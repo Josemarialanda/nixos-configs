97400ff7735ad0c3bd8d92ec68d97efb59af64bc
josemarialanda <josemaria.landa@gmail.com>
2024-11-24 00:49:07 +0000

    add 3840x2160@60hz to sunshine

--- hosts/common/configuration/remote.nix
+++ hosts/common/configuration/remote.nix
@@ -23,7 +23,7 @@
       capSysAdmin = true;
       settings.port = 47989;
       applications.apps = [      
-        { name = "Desktop (2880x1864@60hz)";
+        { name = "2880x1864@60hz";
           prep-cmd = [
             {
               do = ''${pkgs.xorg.xrandr}/bin/xrandr --newmode "2880x1864_60.00"  457.75  2880 3104 3416 3952  1864 1867 1877 1931 -hsync +vsync'';
@@ -35,6 +35,24 @@
             }
             {
               do = ''${pkgs.xorg.xrandr}/bin/xrandr --output DP-1 --mode 2880x1864_60.00'';
+              undo = ''${pkgs.xorg.xrandr}/bin/xrandr --output DP-1 --mode 2560x1440 --rate 144'';
+            }
+          ];
+          exclude-global-prep-cmd = "false";
+          auto-detach = "true";
+        }
+        { name = "3840x2160@60hz";
+          prep-cmd = [
+            {
+              do = ''${pkgs.xorg.xrandr}/bin/xrandr --newmode "3840x2160_60.00"  457.75  2880 3104 3416 3952  1864 1867 1877 1931 -hsync +vsync'';
+              undo = ''${pkgs.xorg.xrandr}/bin/xrandr --rmmode 3840x2160_60.00'';
+            }
+            {
+              do = ''${pkgs.xorg.xrandr}/bin/xrandr --addmode DP-1 3840x2160_60.00'';
+              undo = ''${pkgs.xorg.xrandr}/bin/xrandr --delmode DP-1 3840x2160_60.00'';
+            }
+            {
+              do = ''${pkgs.xorg.xrandr}/bin/xrandr --output DP-1 --mode 3840x2160_60.00'';
               undo = ''${pkgs.xorg.xrandr}/bin/xrandr --output DP-1 --mode 2560x1440 --rate 144'';
             }
           ];
